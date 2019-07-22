2725e5627e348de2cf4257524e67d14d1b66825e
fakersaber <785702978@qq.com>
2019-07-22 17:33:12 +0800

    add Project of Merge Mesh

---
+++ PluginProject/Assets/MeshCombine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MeshCombine : ScriptableObject
+{
+    [MenuItem("Tools/MergeMesh")]
+    static void DoIt()
+    {
+        GameObject[] SelectObjArray = Selection.gameObjects;
+        CombineInstance[] combineInstances = new CombineInstance[SelectObjArray.Length];
+
+        for (int i = 0; i < SelectObjArray.Length; ++i)
+        {
+            var obj = SelectObjArray[i].GetComponent<MeshFilter>();
+            combineInstances[i].mesh = obj.mesh;
+            combineInstances[i].transform = obj.transform.localToWorldMatrix;
+        }
+
+        Mesh CombineMesh = new Mesh();
+        CombineMesh.CombineMeshes(combineInstances);
+        var CombineMeshRender = GameObject.FindWithTag("MeshContainer").AddComponent<MeshFilter>();
+        CombineMeshRender.sharedMesh = CombineMesh;
+    }
+}
